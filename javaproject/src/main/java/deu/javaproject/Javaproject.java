7904b97d23725cf2b7019b22a1091cb157c3a3a9
jj1125 <hanjaejin901@gmail.com>
2024-11-13 14:00:52 +0900

    teat

---
+++ javaproject/src/main/java/deu/javaproject/Javaproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.javaproject;
+
+/**
+ *
+ * @author hanjaejin
+ */
+public class Javaproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
